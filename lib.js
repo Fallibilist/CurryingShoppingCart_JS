db543f9bebc4b96661ab28c9a36b2a37f951aad6
unknown <Greg.Marshall.Hill@gmail.com>
2017-08-31 09:23:17 -0500

    Cleaned up comments and made small conventional changes

--- lib.js
+++ lib.js
@@ -47,29 +47,3 @@ module.exports = {
   cart,
   calculateTotals
 }
-
-// const calculateTotals =
-// listings =>
-//   carts => {
-//     let resultArray = []
-
-//     carts.forEach(cart => {
-//       let cartTotal = 0
-
-//       cart.items.forEach(item => {
-//         cartTotal += listedPrice(listings)(item)
-//       })
-//       resultArray.push({
-//         customer : cart.customer,
-//         total : cartTotal
-//       })
-//     })
-
-//     return resultArray
-//   }
-
-// module.exports = {
-// listing,
-// cart,
-// calculateTotals
-// }
