2520459858788778be733ecca4177387c48ff012
unknown <Greg.Marshall.Hill@gmail.com>
2017-08-31 09:20:55 -0500

    Refactored the Shopping Cart currying project

--- lib.js
+++ lib.js
@@ -33,26 +33,43 @@ const listedPrice =
  */
 const calculateTotals =
   listings =>
-    carts => {
-      let resultArray = []
-
-      carts.forEach(cart => {
-        let cartTotal = 0
-
-        cart.items.forEach(item => {
-          cartTotal += listedPrice(listings)(item)
-        })
-        resultArray.push({
+    carts => 
+      carts.map(
+        cart => ({
           customer : cart.customer,
-          total : cartTotal
+          total : cart.items.reduce(((cartTotal, item) => cartTotal += listedPrice(listings)(item)),0)
         })
-      })
-
-      return resultArray
-    }
+      )
+    
 
 module.exports = {
   listing,
   cart,
   calculateTotals
 }
+
+// const calculateTotals =
+// listings =>
+//   carts => {
+//     let resultArray = []
+
+//     carts.forEach(cart => {
+//       let cartTotal = 0
+
+//       cart.items.forEach(item => {
+//         cartTotal += listedPrice(listings)(item)
+//       })
+//       resultArray.push({
+//         customer : cart.customer,
+//         total : cartTotal
+//       })
+//     })
+
+//     return resultArray
+//   }
+
+// module.exports = {
+// listing,
+// cart,
+// calculateTotals
+// }
